cada9364341e22c30a10ebd4e7e61b8255a17829
kyg9906 <115397550+kyg9906@users.noreply.github.com>
2022-12-01 12:18:40 +0900

    Add files via upload
    made youtube video to fit in.
    incorporated nightdayhandler using jquery and js file.

---
+++ colors.js
@@ -0,0 +1,50 @@
+var Links={
+  setColor:function(color){
+    // var alist=document.querySelectorAll('a');
+    // var i=0;
+    // while(i<alist.length){
+    //   alist[i].style.color=color;
+    //   i=i+1;
+    $('a').css('color', color);
+  }
+}
+  var Borderbottom={
+    setColor: function(color){
+      $('h1').css('border-bottom-color',color);
+    }
+  }
+  var Borderright={
+    setColor:function(color){
+      $('#grid ol').css('border-right-color',color);
+    }
+  }
+
+var Body={
+    setColor:function(color){
+      // document.querySelector('body').style.color=color;
+      $('body').css('color',color);
+  },
+    setBackgroundColor:function(color){
+    // document.querySelector('body').style.backgroundColor=color;
+    $('body').css('backgroundColor',color);
+    }
+}
+
+  function nightDay(self){
+    var target=document.querySelector('body');
+    if(self.value ==='night'){
+      Links.setColor('powderblue');
+      Body.setBackgroundColor('black');
+      Body.setColor('white');
+      Borderbottom.setColor('white');
+      Borderright.setColor('white');
+      self.value = 'day';
+    } else{
+      Links.setColor('black');
+      Body.setBackgroundColor('white');
+      Body.setColor('black');
+      Borderbottom.setColor('black');
+      Borderright.setColor('black');
+      self.value= 'night';
+    }
+  }
